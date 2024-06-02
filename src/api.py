c9ac14473a49543cc65cc3adea3d2f5f1f30cf89
Ajay Bhargava <bhargava.ajay@gmail.com>
2024-06-01 20:48:33 -0400

    init

---
+++ src/api.py
@@ -0,0 +1,27 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from spotify import get_tracks
+
+app = FastAPI()
+
+
+class InputModel(BaseModel):
+    client_id: str
+    client_secret: str
+
+
+@app.post("/tracks/")
+def get_spotify_track(input_model: InputModel):
+    try:
+        track_vibes = get_tracks(
+            client_id=input_model.client_id,
+            client_secret=input_model.client_secret,
+        )
+    except Exception as e:
+        return {"error": str(e)}
+    return track_vibes
+
+
+@app.post("/square/")
+def square(x: int):
+    return {"square": x**2}
---
+++ src/spotify.py
@@ -0,0 +1,51 @@
+import datetime
+
+import spotipy
+import typer
+from rich import print
+from spotipy.oauth2 import SpotifyOAuth
+
+app = typer.Typer()
+
+
+@app.command()
+def get_tracks(
+    client_id: str,
+    client_secret: str,
+):
+    sp = spotipy.Spotify(
+        auth_manager=SpotifyOAuth(
+            client_id=client_id,
+            client_secret=client_secret,
+            redirect_uri="http://localhost:3000",
+            scope=["user-read-recently-played", "user-library-read"],
+        )
+    )
+    # Get the current date and time
+    now = datetime.datetime.now()
+
+    # Set the time to 12am
+    start_time = now.replace(hour=0, minute=0, second=0, microsecond=0)
+
+    # Get the POSIX timestamp of the start time
+    start = int(start_time.timestamp() * 1000)
+    results = sp.current_user_recently_played(after=start)
+
+    parsing = []
+    for idx, item in enumerate(results["items"]):
+        parsing.append(
+            {
+                "spotify_identifier": item["track"]["id"],
+                "track_name": item["track"]["name"],
+                "artist_name": item["track"]["artists"][0]["name"],
+            }
+        )
+
+    identifiers = [item["spotify_identifier"] for item in parsing]
+    print(parsing)
+    track_vibes = sp.audio_features(identifiers)
+    return track_vibes
+
+
+if __name__ == "__main__":
+    app()
